ad2d1df5265d1012487f234a2c3de5e7c8d49a57
Ahzam Akhtar <ahzam8186@gmail.com>
2023-01-06 20:38:27 +0530

    smart contract revision

--- SmartContract/lib.rs
+++ SmartContract/lib.rs
@@ -4,8 +4,6 @@ pub mod constant;
 pub mod states;
 use crate::{constant::*,states::*};
 
-//use create bring things here but to use it inside main function 
-// use usesuper inside clever_todo
 declare_id!("4FN4R2fgVS94QqdCYiuzJVNgmAP2qWU3h9oTqMgs2JeH");
 
 #[program]
@@ -15,9 +13,7 @@ pub mod clever_todo{
     pub fn initialize_user(
         ctx: Context<InitializeUser>
     ) -> Result<()>{
-        //Write Logic Here
-        // Initalize user profile with default data
-        // Here Initialize User is a struct
+      
         let user_profile = &mut ctx.accounts.user_profile;
         user_profile.authority = ctx.accounts.authority.key();
         user_profile.last_todo = 0;
@@ -30,7 +26,6 @@ pub mod clever_todo{
         ctx:Context<AddTodo>,
         _content : String
     ) -> Result<()>{
-        // Fill the todo account with the proper values
         let todo_account = &mut ctx.accounts.todo_account;
         let user_profile = &mut ctx.accounts.user_profile;
 
@@ -38,13 +33,10 @@ pub mod clever_todo{
         todo_account.idx = user_profile.last_todo;
         todo_account.content = _content;
         todo_account.marked = false;
-
-        //Increase todo idx 
         user_profile.last_todo = user_profile.last_todo
         .checked_add(1)
         .unwrap();
 
-        // Increase total todo count 
         user_profile.todo_count = user_profile.todo_count
         .checked_add(1)
         .unwrap();
@@ -61,23 +53,21 @@ pub mod clever_todo{
     }
 
     pub fn remove_todo(ctx:Context<RemoveTodo>,todo_idx:u8) -> Result<()>{
-        //Decrement total todo count
         let user_profile = &mut ctx.accounts.user_profile;
 
         user_profile.todo_count = user_profile.todo_count
         .checked_sub(1).
         unwrap();
-        //No need to decrease the todo idx
         Ok(())
     }
       
 }
 
-// defing InitializeUser struct
+
 #[derive(Accounts)]
 #[instruction()]
 pub struct InitializeUser<'info>{
-    // info is a kind of a  life time variable
+
     #[account(mut)]
     pub authority : Signer<'info>,
 
@@ -95,7 +85,6 @@ pub struct InitializeUser<'info>{
 #[derive(Accounts)]
 #[instruction()]
 pub struct AddTodo<'info>{
-    //getting user profile
     #[account(
         mut,
         seeds = [USER_TAG,authority.key().as_ref()],
--- SmartContract/states.rs
+++ SmartContract/states.rs
@@ -1,5 +1,4 @@
 use anchor_lang::prelude::*;
-//classes
 #[account]
 #[derive(Default)]
 pub struct UserProfile {
